82895a123c49a3b0d7c22eae7e803a2c16cf99ca
HilmiAfifi24 <hilmiafifi64@gmail.com>
2025-01-08 20:53:23 +0700

    terbaru

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-blue-500 font-bold">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
